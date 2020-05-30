206aeebae8db965fe6ce2e977850b79ed46598b3
chikuwa111 <take0shun1111@gmail.com>
2020-05-30 17:54:30 +0900

    build: run `yarn prettier`

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
+import Head from 'next/head';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-const name = "chikuwa111";
-export const siteTitle = "Next.js Sample Website";
+const name = 'chikuwa111';
+export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({
   children,
