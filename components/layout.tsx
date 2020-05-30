709f35ed17ae751a0b2023536ede21ec1aa3af33
chikuwa111 <take0shun1111@gmail.com>
2020-05-30 18:34:43 +0900

    build: run `yarn lint`

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import utilStyles from '../styles/utils.module.css';
+import styles from './layout.module.css';
 
 const name = 'chikuwa111';
 export const siteTitle = 'Next.js Sample Website';
