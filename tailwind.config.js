f4d357c8999fb150ea2b034f9a800919efe5bbf5
chikuwa111 <take0shun1111@gmail.com>
2020-05-31 01:23:20 +0900

    build: Use tailwind purge function instead of using purgecss directly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './constants/style.ts',
+  ],
   theme: {
     extend: {},
   },
